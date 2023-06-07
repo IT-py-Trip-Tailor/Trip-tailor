c679e1ac29ba8a806fd9ab64605aedaa5a8bb61e
Andrew Cosinov <127627981+uShutka@users.noreply.github.com>
2023-06-07 23:47:07 +0300

    second commit by uShuta

--- app.py
+++ app.py
@@ -1,87 +1,94 @@
-from flask import Flask, render_template, request, redirect, make_response
+from flask import Flask, render_template, request, redirect, url_for, make_response
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.exc import IntegrityError
+from werkzeug.security import generate_password_hash, check_password_hash
+import secrets
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = '1111'
 db = SQLAlchemy(app)
+
+    
+def generate_unique_key():
+    # Генерация случайной строки
+    cookie_value = secrets.token_hex(16)
+    return cookie_value
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    unique_key = db.Column(db.String(16))
 
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
-
+# Создание таблицы, если она отсутствует
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
-    # Проверяем, если пользователь уже вошел, перенаправляем на его аккаунт
-    if 'user_id' in request.cookies:
-        return redirect('/account')
+    # Проверка наличия куки с именем пользователя
+    username = request.cookies.get('username')
+    if username:
+        user = User.query.filter_by(username=username).first()
+        return render_template('profile.html', user=user)
     return render_template('index.html')
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        username = request.form['username']
+        # Получение данных из формы регистрации
+        username = request.form['login']
         email = request.form['email']
         password = request.form['password']
-        
-        user = User(username=username, email=email, password=password)
-        db.session.add(user)
+        unique_key = generate_unique_key()
+        # Хэширование пароля
+        hashed_password = generate_password_hash(password)
+        # Создание нового пользователя
+        new_user = User(username=username, email=email, password=hashed_password, unique_key=unique_key)
+        db.session.add(new_user)
+        db.session.commit()
+        # Перенаправление на страницу входа
+        resp = make_response(redirect(url_for('index')))
+        resp.set_cookie('unique_key', unique_key)
+        return resp    
 
-        try:
-            db.session.commit()
-            return redirect('/login')
-        except IntegrityError:
-            db.session.rollback()
-            error_message = 'Пользователь с таким логином или почтой уже существует.'
-            return render_template('register.html', error_message=error_message)
     return render_template('reg.html')
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        login_input = request.form['login_input']
+        # Получение данных из формы входа
+        login = request.form['login']
         password = request.form['password']
+        # Поиск пользователя по имени пользователя или адресу электронной почты
+        user = User.query.filter((User.username == login) | (User.email == login)).first()
+        if user and check_password_hash(user.password, password):
+            # Установка куки с именем пользователя
+            resp = make_response(redirect(url_for('profile')))
+            resp.set_cookie('unique_key', user.unique_key)
+            return resp
+        else:
+            return render_template('login.html', error_message='Неправильно введен логин или пароль')
 
-        user = User.query.filter((User.username == login_input) | (User.email == login_input)).first()
+    error_message = request.args.get('error_message')
+    if error_message == 'not_loginning':
+        return render_template('login.html', error_message='Вы не вошли в аккаунт')
 
-        if user and user.password == password:
-            response = make_response(redirect('/account'))
-            response.set_cookie('user_id', str(user.id))
-            return response
-
-        error_message = 'Неправильное имя пользователя (логин или почта) или пароль.'
-        return render_template('login.html', error_message=error_message)
     return render_template('login.html')
 
 
-@app.route('/account')
-def account():
-    user_id = request.cookies.get('user_id')
-
-    if not user_id:
-        return redirect('/login')
-
-    user = User.query.get(user_id)
-    if not user:
-        return redirect('/login')
-
-    return render_template('account.html', username=user.username)
-
+@app.route('/profile')
+def profile():
+    # Проверка наличия куки с именем пользователя
+    cookie = request.cookies.get('unique_key')
+    if cookie:
+        # Поиск пользователя по имени пользователя
+        user = User.query.filter_by(unique_key=cookie).first()
+        if user:
+            return render_template('profile.html', user=user.username)
+    # Если куки с именем пользователя отсутствуют или пользователь не найден, перенаправляем на страницу входа
+    return redirect(url_for('login', error_message='not_loginning'))
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
