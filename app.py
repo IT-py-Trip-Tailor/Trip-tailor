11bd8a581bca6aa8bc9f42935c5b80e774032135
Andrew Cosinov <127627981+uShutka@users.noreply.github.com>
2023-06-10 20:40:25 +0300

    soft update by uShuta

--- app.py
+++ app.py
@@ -152,5 +152,22 @@ def info():
 def tour_info():
     return render_template('tour_info.html')
 
+@app.route('/tour_package', methods=['GET', 'POST'])
+def tour_package():
+    if request.method == 'POST':
+        from_ = request.form.get('from')
+        where = request.form.get('where')
+        date1 = request.form.get('date1')
+        date2 = request.form.get('date2')
+        cost1 = request.form.get('cost1')
+        cost2 = request.form.get('cost2')
+        route = request.form.get('route')
+        target = request.form.get('target')
+        type_tour = request.form.get('type_tour')
+        transfer1 = request.form.get('transfer1')
+        transfer2 = request.form.get('transfer2')
+
+    return render_template('tour_package.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
